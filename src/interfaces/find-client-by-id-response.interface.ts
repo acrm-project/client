d43846df450ba8ab8c5cac05a9d870e785f656d6
amIAqua <sidelnikov224@gmail.com>
2021-07-17 10:42:53 +0300

    types

--- src/interfaces/find-client-by-id-response.interface.ts
+++ src/interfaces/find-client-by-id-response.interface.ts
@@ -1,8 +1,9 @@
 import { HttpStatus } from '@nestjs/common'
+import { Error } from 'src/types/common/error.type'
 import { IClient } from './client.interface'
 
 export interface IFindClientByIdResponse {
   client?: IClient
   status: HttpStatus
-  error?: string | null
+  error?: Error
 }
